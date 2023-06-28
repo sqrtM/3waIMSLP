8eebf080e49fc35b212fdae2db757166f9a123ad
Mason Pike <mpike@flyamelia.com>
2023-06-28 16:36:01 +0200

    feat: added getByIndex to searchService

--- 3waimslp/tests/SearchServiceTest.php
+++ 3waimslp/tests/SearchServiceTest.php
@@ -12,7 +12,7 @@ class SearchServiceTest extends TestCase
 
     private SearchService $search;
 
-    public function testSearchForMusicAlgorithmWorks()
+    public function testSearchForMusicAlgorithm()
     {
         $this->search = new SearchService();
         $results = $this->search->searchForMusic("Cembalo", 2);
@@ -20,11 +20,27 @@ class SearchServiceTest extends TestCase
         $this->assertIsArray($results);
     }
 
-    public function testSearchForComposerAlgorithmWorks()
+    public function testSearchForComposerAlgorithm()
     {
         $this->search = new SearchService();
         $results = $this->search->searchForComposer("Andreini", 3);
         fwrite(STDERR, print_r($results, TRUE));
         $this->assertIsArray($results);
+    }
+
+    public function testGetMusicByIndex() 
+    {
+        $this->search = new SearchService();
+        $results = $this->search->getMusicByIndex(378);
+        fwrite(STDERR, print_r($results, TRUE));
+        $this->assertEquals("10 Deutsche Tänze mit Coda (Vocet, Ignác)", $results["id"]);
+    }
+
+    public function testGetComposerByIndex() 
+    {
+        $this->search = new SearchService();
+        $results = $this->search->getComposerByIndex(378);
+        fwrite(STDERR, print_r($results, TRUE));
+        $this->assertEquals("Category:Akhmatova, Anna", $results["id"]);
     }
 }
