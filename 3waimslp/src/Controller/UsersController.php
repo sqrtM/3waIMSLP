cfd32d05b82a0cc3733ee2797f238d1ef6cfba6f
Rodo-Gala <Rodolphe.davidg@gmail.com>
2023-06-28 15:29:57 +0200

    Register & login

---
+++ 3waimslp/src/Controller/UsersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Repository\UsersRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/users')]
+class UsersController extends AbstractController
+{
+    #[Route('/', name: 'app_users_index', methods: ['GET'])]
+    public function index(UsersRepository $usersRepository): Response
+    {
+        return $this->render('users/index.html.twig', [
+            'users' => $usersRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_users_show', methods: ['GET'])]
+    public function show(Users $user): Response
+    {
+        return $this->render('users/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
