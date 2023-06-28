9ecaaec2b1f84338a96f0bb9afd7b5cfc6441234
mason <79169638+sqrtM@users.noreply.github.com>
2023-06-28 15:37:34 +0200

    Searchalgo (#2)
    feat: music and composer search algorithms complete

--- 3waimslp/src/Service/SearchService.php
+++ 3waimslp/src/Service/SearchService.php
@@ -8,24 +8,77 @@ class SearchService {
     public function __construct() {
     }
 
-    public function callApiForMusic(int $start): string {
-        return file_get_contents("https://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/sort=id/type=2/start=$start/limit=1000/retformat=json");
-    }
-
-    public function callApiForComposer(int $start): string {
-        return file_get_contents("https://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/sort=id/type=1/start=$start/limit=1000/retformat=json");
-    }
-
-
-    public function searchForTargetMusic(string $target) {
-        // API has 224063 entries in music, so take a guess as to a good starting spot
-        // for now we start in the middle
-        $json = $this->callApiForMusic(110000);
-        if (strpos($json, $target) === false) {
-            return json_decode($json, associative: true)[0]["intvals"]["worktitle"];
-        } else {
-            return "hello";
+    public function searchForMusic(string $searchTerm, int $iterations) {
+        $results = [];
+        for ($i = 0; $i < $iterations; $i++) {
+            $response = $this->searchApiForTargetMusic($searchTerm, $i * 1000);
+            if (!is_null($response)) {
+                array_push($results, ...$response);
+            }
         }
-    }   
+        return $results;
+    }
+
+    public function searchForComposer(string $searchTerm, int $iterations) {
+        $results = [];
+        for ($i = 0; $i < $iterations; $i++) {
+            $response = $this->searchApiForTargetComposer($searchTerm, $i * 1000);
+            if (!is_null($response)) {
+                array_push($results, ...$response);
+            }
+        }
+        return $results;
+    }
+
+    private function callApiForMusic(int $start): string {
+        return file_get_contents("https://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/sort=id/type=2/start=".$start."/limit=1000/retformat=json");
+    }
+
+    private function callApiForComposer(int $start): string {
+        return file_get_contents("https://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/sort=id/type=1/start=".$start."/limit=1000/retformat=json");
+    }
+
+
+    private function searchApiForTargetMusic(string $target, int $start) {
+        $json = $this->callApiForMusic($start);
+        // if we can't find a match .... 
+        if (!strpos($json, $target)) {
+            return null;
+        } else {
+            $arr = json_decode($json, associative: true);
+            array_pop($arr); // remove metadata from array
+            $returnArr = [];
+            for ($i = 0; $i < count($arr); $i++) {
+                if (strpos($arr[$i]["intvals"]["worktitle"], $target) !== false) {
+                    if ($i - 1 >= 0) array_push($returnArr, $arr[$i - 1]);
+                    array_push($returnArr, $arr[$i]);
+                    if ($i + 1 < 1000) array_push($returnArr, $arr[$i + 1]);
+                    if ($i + 2 < 1000) array_push($returnArr, $arr[$i + 2]);
+                }
+            }
+            return $returnArr;
+        }
+    }
+
+    private function searchApiForTargetComposer(string $target, int $start) {
+        $json = $this->callApiForComposer($start);
+        // if we can't find a match .... 
+        if (!strpos($json, $target)) {
+            return null;
+        } else {
+            $arr = json_decode($json, associative: true);
+            array_pop($arr); // remove metadata from array
+            $returnArr = [];
+            for ($i = 0; $i < count($arr); $i++) {
+                if (strpos($arr[$i]["id"], $target) !== false) {
+                    if ($i - 1 >= 0) array_push($returnArr, $arr[$i - 1]);
+                    array_push($returnArr, $arr[$i]);
+                    if ($i + 1 < 1000) array_push($returnArr, $arr[$i + 1]);
+                    if ($i + 2 < 1000) array_push($returnArr, $arr[$i + 2]);
+                }
+            }
+            return $returnArr;
+        }
+    }
 }
 
--- 3waimslp/tests/SearchServiceTest.php
+++ 3waimslp/tests/SearchServiceTest.php
@@ -1,26 +1,31 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 namespace Tests;
 
 use App\Service\SearchService;
+use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
 
-class SearchServiceTest extends TestCase {
+class SearchServiceTest extends TestCase
+{
 
     private SearchService $search;
 
-    public function testSearchServiceCallsApi() {
+    public function testSearchForMusicAlgorithmWorks()
+    {
         $this->search = new SearchService();
-        $jsonRes = $this->search->callApiForMusic(0);
-        $this->assertIsInt(strpos($jsonRes, "Amicizia"));
+        $results = $this->search->searchForMusic("Cembalo", 2);
+        fwrite(STDERR, print_r($results, TRUE));
+        $this->assertIsArray($results);
     }
 
-    public function testSearchAlgorithmWorks() {
+    public function testSearchForComposerAlgorithmWorks()
+    {
         $this->search = new SearchService();
-        $jsonRes = $this->search->searchForTargetMusic("mahler");
-        fwrite(STDERR, print_r($jsonRes, TRUE));
-        $this->assertIsInt(5);
+        $results = $this->search->searchForComposer("Andreini", 3);
+        fwrite(STDERR, print_r($results, TRUE));
+        $this->assertIsArray($results);
     }
 }
