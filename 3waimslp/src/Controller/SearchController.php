da0c379cb64fd22f575fc9aa4912c1b34e437bf0
Rodo-Gala <Rodolphe.davidg@gmail.com>
2023-06-29 14:34:20 +0200

    Data result page

--- 3waimslp/src/Controller/SearchController.php
+++ 3waimslp/src/Controller/SearchController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Favorites;
 use App\Form\SearchType;
+use App\Service\ComposerSearch;
+use App\Service\MusicSearch;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,34 +17,28 @@ use App\Service\SearchService;
 
 class SearchController extends AbstractController
 {
-    public function __construct(SearchService $searchService, EntityManagerInterface $entityManager){
-
-    $this->searchService = $searchService;
-    $this->entityManager = $entityManager;
-
-    }
-
-    #[Route('/search', name: 'app_search')]
-    public function index(Request $request): Response
-    {
-
-        $form = $this->createForm(SearchType::class);
-
-        $form->handleRequest($request);
-
-        return $this->render('search/search.html.twig', [
-            'form' => $form->createView(),
-        ]);
+    public function __construct(SearchService $searchService, EntityManagerInterface $entityManager, ComposerSearch $composerSearch, MusicSearch $musicSearch){
+        $this->searchService = $searchService;
+        $this->entityManager = $entityManager;
+        $this->composerSearch = $composerSearch;
+        $this->musicSearch = $musicSearch;
     }
 
     #[Route('/search/results', name: 'app_search_results')]
     public function results(Request $request): Response
     {
         //get data from request
-        dd($request->query);
+        $get = $request->query->all();
+
+        $musics =  $this->musicSearch->search($get['search']['search'], 5);
+        $composers = $this->composerSearch->search($get['search']['search'], 5);
+
+        $data = array_merge($musics, $composers);
 
         //Call api with data to get results
-        return $this->render('search/results.html.twig');
+        return $this->render('search/results.html.twig', [
+            'datas' => $data
+        ]);
     }
 
     //TODO Mettre id en {id} où seras notre imslpIndex pour retrouver la ressource
